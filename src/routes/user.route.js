554c50780020da2eaacb97d97e2a787041fed6f2
Kira-Window <duycoder544@gmail.com>
2026-05-27 10:52:37 +0700

    [feat] document api - swagger

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,7 +3,26 @@ import { getUserProfile } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-// Khi client gọi GET: /api/v1/users/profile
+/**
+ * @swagger
+ * /profile:
+ *   get:
+ *     summary: Lấy thông tin cá nhân user
+ *     responses:
+ *       200:
+ *         description: Thành công
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ */
 router.get('/profile', getUserProfile);
 
 export default router;
