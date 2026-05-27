6379989433630809aea3fc1868c059ca99733862
Kira-Window <duycoder544@gmail.com>
2026-05-27 22:07:55 +0700

    Fix: Resolve merge conflict in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,13 +10,11 @@ const router = express.Router();
 
 // Gom router của user vào đường dẫn /users
 router.use('/users', userRouter);
+router.use('/projects', projectRouter);
 
 // Gom router của login, register và google vào đường dẫn /auth
 router.use('/auth', loginRouter);
 router.use('/auth', registerRouter);
 router.use('/auth', googleRouter);
-
-// Gom router của project vào đường dẫn /projects
-router.use('/projects', projectRouter);
 
 export default router;
