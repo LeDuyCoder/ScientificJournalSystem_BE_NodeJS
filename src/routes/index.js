020243aca3aa9b3347e45c2c5aef9ff191f6ce73
KalvinKhanh <tranminhkhanh793@gmail.com>
2026-05-28 20:19:13 +0700

    feat: add/list keyword trending API

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,23 @@
-import express from 'express';
-import userRouter from './user.route.js';
-import loginRouter from './login.route.js';
-import registerRouter from './register.route.js';
-import projectRouter from './project.route.js';
-import googleRouter from './google.route.js';
-
-
+import express from "express";
+import userRouter from "./user.route.js";
+import loginRouter from "./login.route.js";
+import registerRouter from "./register.route.js";
+import projectRouter from "./project.route.js";
+import googleRouter from "./google.route.js";
+import keywordRouter from "./keyword.route.js";
 const router = express.Router();
 
 // Gom router của user vào đường dẫn /users
-router.use('/users', userRouter);
+router.use("/users", userRouter);
 
 // Gom router của login, register và google vào đường dẫn /auth
-router.use('/auth', loginRouter);
-router.use('/auth', registerRouter);
-router.use('/auth', googleRouter);
+router.use("/auth", loginRouter);
+router.use("/auth", registerRouter);
+router.use("/auth", googleRouter);
 
 // Gom router của project vào đường dẫn /projects
-router.use('/projects', projectRouter);
+router.use("/projects", projectRouter);
 
+// Gom router của keyword vào đường dẫn /projects
+router.use("/projects", keywordRouter); // ← thêm dòng này
 export default router;
